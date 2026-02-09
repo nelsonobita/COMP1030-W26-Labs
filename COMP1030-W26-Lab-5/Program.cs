446284d6ba984fefb383613fb2d1874701eba3af
NelsonObita <nelsonobita@protonmail.com>
2026-02-08 22:21:38 -0500

    Added outer row loop (1-10)

---
+++ COMP1030-W26-Lab-5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int size = 10;
+
+        // Outer loop (FOR): rows 1 to 10
+        for (int row = 1; row <= size; row++)
+        {

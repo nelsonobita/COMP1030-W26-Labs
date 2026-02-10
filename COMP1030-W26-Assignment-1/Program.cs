66ce7018f7aadfc5e678b130845833898450f279
NelsonObita <nelsonobita@protonmail.com>
2026-02-09 22:00:22 -0500

    added three flooring options (strings) and a variable to capture the user flooring choice

--- COMP1030-W26-Assignment-1/Program.cs
+++ COMP1030-W26-Assignment-1/Program.cs
@@ -4,3 +4,13 @@ class Program
 {
     static void Main()
     {
+        // Assignment 1 -  C# Flooring program
+        // Floor room calculating program
+
+// step 2: Three flooring options (strings) that include price per sq. ft.
+        string option1 = "Option 1: Hardwood - $5.00 per sq. ft.";
+        string option2 = "Option 2: Laminate - $3.00 per sq. ft.";
+        string option3 = "Option 3: Carpet - $2.00 per sq. ft.";
+
+        // STEP 3: Variable to capture the user's flooring choice
+        string choice = "";

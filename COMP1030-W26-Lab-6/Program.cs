dac508b6d8ca05ef18069a494d05d20998e4516b
NelsonObita <nelsonobita@protonmail.com>
2026-03-30 13:26:01 -0400

    Add ice cream flavors array and length variable

---
+++ COMP1030-W26-Lab-6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP1030_W26_Lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 1 - array of ice cream flavors (picked 8)
+            string[] iceCreamFlavors = { "Vanilla", "Chocolate", "Strawberry", "Mint Chip", "Cookie Dough", "Rocky Road", "Butter Pecan", "Mango Sorbet" };
+
+            // step 2 - storing the length of the array
+            int flavorCount = iceCreamFlavors.Length;

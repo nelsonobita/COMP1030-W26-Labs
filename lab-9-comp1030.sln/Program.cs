77770f331968016a108a2706b3ad98199d6d7dcd
NelsonObita <nelsonobita@protonmail.com>
2026-03-30 16:12:22 -0400

    Add rectangle2 instance with description and draw

--- lab-9-comp1030.sln/Program.cs
+++ lab-9-comp1030.sln/Program.cs
@@ -15,5 +15,17 @@ class Program
 
         // step 4c - drawing rectangle1 to the console
         rectangle1.DrawRectangle();
+
+        // step 6 - creating rectangle2 with different height and width
+        var rectangle2 = new Rectangle(3, 15);
+
+        // step 7a - getting the description of rectangle2
+        string description2 = rectangle2.DescribeRectangle();
+
+        // step 7b - outputting the description
+        Console.WriteLine(description2);
+
+        // step 7c - drawing rectangle2 to the console
+        rectangle2.DrawRectangle();
     }
 }

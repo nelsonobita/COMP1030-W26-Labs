e279dea394e98d4951afd56a86f1944b11d2d4e3
NelsonObita <nelsonobita@protonmail.com>
2026-03-30 15:38:26 -0400

    Add Lab9.cs with Ball class and properties

---
+++ lab-9.sln/Lab9.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab9
+{
+    // step 5 - creating the Ball class
+    class Ball
+    {
+        // step 6 - the 3 properties of a ball
+        public string Color;
+        public int Size;
+        public string Material;
+    }
+}

02643cdf35004289281654867931750812147de3
NelsonObita <nelsonobita@protonmail.com>
2026-03-30 13:47:41 -0400

    Add FOR loop to output flavors (after Steps 3–5)

--- COMP1030-W26-Lab-6/Program.cs
+++ COMP1030-W26-Lab-6/Program.cs
@@ -9,3 +9,12 @@
 
             // step 2 - storing the length of the array
             int flavorCount = iceCreamFlavors.Length;
+
+            // STEP 3: Output intro sentence for the FOR() loop section
+            Console.WriteLine("Here are all the ice cream flavors available at our stand (using a FOR loop):");
+
+            // STEP 4 & 5: Build a FOR() loop using flavorCount to control iterations, output each element
+            for (int i = 0; i < flavorCount; i++)
+            {
+                Console.WriteLine(iceCreamFlavors[i]);
+            }

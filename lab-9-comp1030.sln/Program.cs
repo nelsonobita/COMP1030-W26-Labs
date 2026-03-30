7e90a68a73569670153f8a00dd97d8c9fc35a27c
NelsonObita <nelsonobita@protonmail.com>
2026-03-30 16:11:01 -0400

    Add rectangle1 instance with description and draw

--- lab-9-comp1030.sln/Program.cs
+++ lab-9-comp1030.sln/Program.cs
@@ -1,9 +1,19 @@
-﻿// step 1 - declaring Shapes as a dependency
-using Shapes;
+﻿using Shapes;
 
 class Program
 {
     static void Main()
     {
+        // step 3 - creating rectangle1 with a height and width
+        var rectangle1 = new Rectangle(5, 10);
+
+        // step 4a - getting the description of rectangle1
+        string description1 = rectangle1.DescribeRectangle();
+
+        // step 4b - outputting the description
+        Console.WriteLine(description1);
+
+        // step 4c - drawing rectangle1 to the console
+        rectangle1.DrawRectangle();
     }
 }
